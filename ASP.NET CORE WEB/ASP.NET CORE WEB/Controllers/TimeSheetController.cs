d84d856559af7d91ce85e4baf2329d4699702707
Moreart1 <morearti1726263@yandex.ru>
2022-04-08 16:07:16 +0300

    1 Расписать сущности для проекта Timesheets 2 Расписать API для проект Timesheets

---
+++ ASP.NET CORE WEB/ASP.NET CORE WEB/Controllers/TimeSheetController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_CORE_WEB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeSheetController : ControllerBase
+    {
+        
+   
+    
+    }
+}
